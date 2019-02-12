844cf85f96c52855b6c74a04254048543dc50c94
Mike <malphas@subdimension.co.uk>
2019-02-12 12:52:07 +0000

    initial rework for Spike

--- main.c
+++ main.c
@@ -2,307 +2,105 @@
 
 #include "main.h"
 
-word t = 0;
-//word btn_timer = 0;
+dword t = 0;
+dword btn_timer = 0;
 
-typedef struct sprite {
-    byte glyph;
-    int x;
-    int y;
-    byte row;
-    byte y_offset;
-    
-    int xv;
-    int yv;
-    
-    word timer;
-} sprite;
+Mob mobs[MAX_MOBS];
 
-word delta_sum = 0;
-word delta_count = 0;
-byte delta = 0;
-
-byte HUD[4*8];
-byte fps=0;
-
-byte buffer[128];
+byte buttons;
 
 int main (void) 
 {    
     initialise();
     
-    display_image(&LOGO[0], 3*8, 3, 10, 2);
-    beep(_A4, 120);
-    delay_ms(125);
-    beep(_C5, 90), 
-    delay_ms(95);
-    beep(_E5, 60);
-    
-    delay_ms(SPLASH_DELAY-(125+95+60));
-    
-    sprite sprites[MAX_SPRITES] = {
-        {.glyph=0, .x=0, .y=0, .xv=3, .yv=0, .timer=0},
-        {.glyph=0, .x=0, .y=0, .xv=3, .yv=0, .timer=0},
-        {.glyph=0, .x=0, .y=0, .xv=3, .yv=0, .timer=0},
-        {.glyph=0, .x=0, .y=0, .xv=3, .yv=0, .timer=0},
-        {.glyph=0, .x=0, .y=0, .xv=3, .yv=0, .timer=0},
-        {.glyph=0, .x=0, .y=0, .xv=3, .yv=0, .timer=0},
-        {.glyph=0, .x=0, .y=0, .xv=3, .yv=0, .timer=0},
-            
-        {.glyph=ENEMY_SHIP, .x=13*8, .y=4+(0*8), .xv=-1, .yv=0, .timer=0},
-        {.glyph=ENEMY_SHIP, .x=12*8, .y=4+(1*8), .xv=-1, .yv=0, .timer=0},
-        {.glyph=ENEMY_SHIP, .x=11*8, .y=4+(2*8), .xv=-1, .yv=0, .timer=0},
-        {.glyph=ENEMY_SHIP, .x=10*8, .y=4+(3*8), .xv=-1, .yv=0, .timer=0},
-        {.glyph=ENEMY_SHIP, .x=11*8, .y=4+(4*8), .xv=-1, .yv=0, .timer=0},
-        {.glyph=ENEMY_SHIP, .x=12*8, .y=4+(5*8), .xv=-1, .yv=0, .timer=0},
-        {.glyph=ENEMY_SHIP, .x=13*8, .y=4+(6*8), .xv=-1, .yv=0, .timer=0},
-        {.glyph=0, .x=0, .y=0, .xv=0, .yv=0},
-        
-        {.glyph=PLAYER_SHIP, .x=2, .y=2*8, .xv=0, .yv=0, .timer=0},
-    };
-    
-    sprite *player = &sprites[MAX_SPRITES-1];
-    
-    /*byte wave = 1;
-    
-    clear_display();
-    
-    set_display_col_row(6*8, 4);
-    shift_out_block(&GLYPHS[WA], FALSE);
-    shift_out_block(&GLYPHS[VE], FALSE);
-    shift_out_block(&GLYPHS[0], FALSE);
-    shift_out_block(&GLYPHS[(wave+1)*8], FALSE);
-    
-    delay_ms(1500);*/
+    byte debounce = 0;
+    bool button_down = FALSE;
     
     for(ever)
     {
         t = millis();
         
-        if (player->timer < t)
+        for (word i=0 ; i<(SCREEN_WIDTH*SCREEN_ROWS) ; i++)
+            buffer[i] = TITLE[i];
+        
+        draw();
+        
+        buttons = ~PINC;
+        if(buttons == _A && !button_down)
         {
-            word btn_val = read_buttons();
-            
-            // TODO: Need to map values with A held down
-            if (btn_val >= _UP-BTN_THRESHOLD && btn_val <= _UP+BTN_THRESHOLD)
-            {
-                player->y -= SPEED;
-                player->timer = t+FRAME_DELAY;
-            }
-            else if(btn_val >= _DOWN-BTN_THRESHOLD && btn_val <= _DOWN+BTN_THRESHOLD)
-            {
-                player->y += SPEED;
-                player->timer = t+FRAME_DELAY;
-            }
-            else if(btn_val >= _LEFT-BTN_THRESHOLD && btn_val <= _LEFT+BTN_THRESHOLD)
-            {
-                player->x -= SPEED;
-                player->timer = t+FRAME_DELAY;
-            }
-            else if(btn_val >= _RIGHT-BTN_THRESHOLD && btn_val <= _RIGHT+BTN_THRESHOLD)
-            {
-                player->x += SPEED;
-                player->timer = t+FRAME_DELAY;
-            }
-            else if(btn_val >= _A-BTN_THRESHOLD && btn_val <= _A+BTN_THRESHOLD)
+            button_down = TRUE;
+            debounce = t+10;
+        }
+        
+        if (buttons != _A && button_down && debounce <= t)
+        {
+            button_down = FALSE;
+            argon();
+        }
+    }
+}
+
+void argon(void)
+{
+    
+    mobs[0] = (Mob){
+        .vx=0,
+        .vy=0,
+        .sprite = (Sprite){.tile=&TILES[PLAYER_SHIP], .mask=&MASKS[PLAYER_SHIP], .x=2, .y=32-4,},
+        .dead=FALSE,
+    };
+    
+    Mob *player = &mobs[0];
+    
+    for(ever)
+    {
+        t = millis();
+        
+        buttons = ~PINC;
+        if (btn_timer <= t)
+        {
+            if ( buttons & _A )
             {
                 click();
-                for (byte s=0 ; s < 4 ; s++)
-                {
-                    if (sprites[s].glyph == 0 && player->x < 120)
-                    {
-                        sprites[s].glyph = PLASMA_BOLT;
-                        sprites[s].x = player->x+8;
-                        sprites[s].y = player->y;
-                        
-                        break;
-                    }
-                }
-                
-                player->timer = t+FRAME_DELAY;
             }
-            /*else if(btn_val >= _B-BTN_THRESHOLD && btn_val <= _B+BTN_THRESHOLD)
+            
+            if ( buttons & _UP )
             {
-                //click();
-                btn_timer = t+BTN_DELAY;
+                player->sprite.y -= PLAYER_SPEED;
+                if (player->sprite.y < 0)
+                    player->sprite.y += PLAYER_SPEED;
             }
-            else if(btn_val >= _C-BTN_THRESHOLD && btn_val <= _C+BTN_THRESHOLD)
+            if ( buttons & _DOWN )
             {
-                //click();
-                btn_timer = t+BTN_DELAY;
-            }*/
+                player->sprite.y += PLAYER_SPEED;
+                if (player->sprite.y > SCREEN_HEIGHT-8)
+                    player->sprite.y -= PLAYER_SPEED;
+            }
+            if ( buttons & _LEFT )
+            {
+                player->sprite.x -= PLAYER_SPEED;
+                if (player->sprite.x < 0)
+                    player->sprite.x += PLAYER_SPEED;
+            }
+            if ( buttons & _RIGHT )
+            {
+                player->sprite.x += PLAYER_SPEED;
+                if (player->sprite.x > SCREEN_WIDTH-8)
+                    player->sprite.x -= PLAYER_SPEED;
+            }
+            
+            //TODO: lazy
+            btn_timer = t+20;
         }
         
-        //if (t > btn_timer)
-        //    btn_timer = 0;
+        clear_buffer();
         
-        /* Update Sprites */
-        byte active_enemies = 0;
-        for(byte s=0 ; s<MAX_SPRITES ; s++)
+        for (byte i=0 ; i<MAX_MOBS ; i++)
         {
-            if (sprites[s].timer < t)
-            {
-                sprites[s].x += sprites[s].xv;
-                sprites[s].y += sprites[s].yv;
-                sprites[s].timer = t+FRAME_DELAY;
-            }
-            
-            if (sprites[s].x < 0)
-                sprites[s].x = 0;
-            else if (sprites[s].x > SCREEN_WIDTH-8)
-                sprites[s].x = SCREEN_WIDTH-8;
-            
-            if (sprites[s].y < 0)
-                sprites[s].y = 0;
-            else if (sprites[s].y > SCREEN_HEIGHT-8)
-                sprites[s].y = SCREEN_HEIGHT-8;
-            
-            sprites[s].row = sprites[s].y/8;
-            sprites[s].y_offset = sprites[s].y%8;
-            
-            // Collision detection
-            for(byte _s=0 ; _s<MAX_SPRITES ; _s++)
-            {
-                // Don't check self! Also, sprites can't collide with other sprites of same type
-                if (s != _s && 
-                    sprites[s].glyph != sprites[_s].glyph && 
-                    sprites[s].glyph != PLAYER_SHIP &&          // TODO: 
-                    sprites[_s].glyph != PLAYER_SHIP && 
-                    sprites[s].glyph != 0 &&
-                    sprites[_s].glyph != 0)
-                {
-                    if (sprites[s].x >= sprites[_s].x && sprites[s].x < sprites[_s].x+8 &&
-                        sprites[s].y >= sprites[_s].y && sprites[s].y < sprites[_s].y+8)
-                    {
-                        sprites[s].glyph=0;
-                        sprites[s].x = 0;
-                        sprites[s].y = 0;
-                        
-                        sprites[_s].glyph=0;
-                        sprites[_s].x = 0;
-                        sprites[_s].y = 0;
-                    }
-                }
-            }
-            
-            if (sprites[s].glyph == PLASMA_BOLT)
-            {
-                
-                if (sprites[s].x >= SCREEN_WIDTH-8)
-                {
-                    sprites[s].glyph = 0;
-                    sprites[s].x = 0;
-                    sprites[s].y = 0;
-                }
-            }
-            else if (sprites[s].glyph == ENEMY_SHIP)
-            {
-                active_enemies += 1;
-                
-                if (sprites[s].x == 0 || sprites[s].x == SCREEN_WIDTH-8)
-                {
-                    sprites[s].xv *= -1;
-                    if (sprites[s].y > 32)
-                        sprites[s].yv = 1;
-                    else
-                        sprites[s].yv = -1;
-                }
-                
-                if (sprites[s].y == 0 || sprites[s].y == SCREEN_HEIGHT-8)
-                {
-                    //TODO: Do something cleverer here!
-                    sprites[s].yv *= -1;
-                }
-            }
+            if (!mobs[i].dead)
+                draw_sprite(&mobs[i].sprite);
         }
-        
-        /*if (active_enemies == 0)
-        {
-            wave += 1;
             
-            set_display_col_row(7*8, 3);
-            shift_out_block(&GLYPHS[WA], FALSE);
-            shift_out_block(&GLYPHS[VE], FALSE);
-            shift_out_block(&GLYPHS[0], FALSE);
-            shift_out_block(&GLYPHS[(wave+1)*8], FALSE);
-            
-            delay_ms(1500); //TODO: Don't delay - use a timer.
-            
-            //TODO: Set up params for next wave.
-        }*/
-        
-        /* DRAW LOOP */
-        set_display_col_row(0, 0);
-        for (byte row=0 ; row<SCREEN_ROWS ; row++)
-        {
-            for (byte col=0 ; col<SCREEN_COLUMNS ; col++)
-            {
-                buffer[col*8+0] = GLYPHS[MAP[ SCREEN_COLUMNS * row + col ]*8 + 0];
-                buffer[col*8+1] = GLYPHS[MAP[ SCREEN_COLUMNS * row + col ]*8 + 1];
-                buffer[col*8+2] = GLYPHS[MAP[ SCREEN_COLUMNS * row + col ]*8 + 2];
-                buffer[col*8+3] = GLYPHS[MAP[ SCREEN_COLUMNS * row + col ]*8 + 3];
-                buffer[col*8+4] = GLYPHS[MAP[ SCREEN_COLUMNS * row + col ]*8 + 4];
-                buffer[col*8+5] = GLYPHS[MAP[ SCREEN_COLUMNS * row + col ]*8 + 5];
-                buffer[col*8+6] = GLYPHS[MAP[ SCREEN_COLUMNS * row + col ]*8 + 6];
-                buffer[col*8+7] = GLYPHS[MAP[ SCREEN_COLUMNS * row + col ]*8 + 7];
-            }
-            
-            for(byte s=0 ; s<MAX_SPRITES ; s++)
-            {
-                if (row == sprites[s].row)
-                {
-                    buffer[sprites[s].x+0] |= GLYPHS[sprites[s].glyph*8+0] >> sprites[s].y_offset;
-                    buffer[sprites[s].x+1] |= GLYPHS[sprites[s].glyph*8+1] >> sprites[s].y_offset;
-                    buffer[sprites[s].x+2] |= GLYPHS[sprites[s].glyph*8+2] >> sprites[s].y_offset;
-                    buffer[sprites[s].x+3] |= GLYPHS[sprites[s].glyph*8+3] >> sprites[s].y_offset;
-                    buffer[sprites[s].x+4] |= GLYPHS[sprites[s].glyph*8+4] >> sprites[s].y_offset;
-                    buffer[sprites[s].x+5] |= GLYPHS[sprites[s].glyph*8+5] >> sprites[s].y_offset;
-                    buffer[sprites[s].x+6] |= GLYPHS[sprites[s].glyph*8+6] >> sprites[s].y_offset;
-                    buffer[sprites[s].x+7] |= GLYPHS[sprites[s].glyph*8+7] >> sprites[s].y_offset;
-                }
-                else if (row == sprites[s].row+1 && sprites[s].y_offset > 0)
-                {
-                    buffer[sprites[s].x+0] |= GLYPHS[sprites[s].glyph*8+0] << (8-sprites[s].y_offset);
-                    buffer[sprites[s].x+1] |= GLYPHS[sprites[s].glyph*8+1] << (8-sprites[s].y_offset);
-                    buffer[sprites[s].x+2] |= GLYPHS[sprites[s].glyph*8+2] << (8-sprites[s].y_offset);
-                    buffer[sprites[s].x+3] |= GLYPHS[sprites[s].glyph*8+3] << (8-sprites[s].y_offset);
-                    buffer[sprites[s].x+4] |= GLYPHS[sprites[s].glyph*8+4] << (8-sprites[s].y_offset);
-                    buffer[sprites[s].x+5] |= GLYPHS[sprites[s].glyph*8+5] << (8-sprites[s].y_offset);
-                    buffer[sprites[s].x+6] |= GLYPHS[sprites[s].glyph*8+6] << (8-sprites[s].y_offset);
-                    buffer[sprites[s].x+7] |= GLYPHS[sprites[s].glyph*8+7] << (8-sprites[s].y_offset);
-                }
-            }
-            
-            /* Display HUD */
-            if (row == 7)
-            {
-                for (byte i=0 ; i<4*8 ; i++)
-                    buffer[i] = HUD[i];
-            }
-            
-            for (byte x=0 ; x<SCREEN_WIDTH ; x++)
-                shift_out_byte(buffer[x]);
-        }
-        
-        /* Update Framerate (DEBUGGING) */
-        delta_sum += millis() - t;
-        delta_count += 1;
-        if (delta_count == 25) // Update every 25 frames
-        {
-            delta = delta_sum / delta_count;
-            if (delta != 0)
-                fps = 1000 / delta;
-            
-            delta_sum = 0;
-            delta_count = 0;
-            byte digit = 0;
-            for (byte d=0 ; d<4 ; d++)
-            {
-                digit = ((fps % 10)+1)*8;
-                for(byte i=0 ; i<8 ; i++)
-                    HUD[((3-d)*8)+i] = GLYPHS[digit+i];
-                fps = fps / 10;
-            }
-        }
+        draw();
     }
 }
